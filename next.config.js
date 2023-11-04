e08e6a56a9c8f9671a57cba7f2aa6472b2b81a50
christina <tochukwu.ngene@sintaxy.com>
2023-11-04 19:55:22 -0100

    content18

--- next.config.js
+++ next.config.js
@@ -3,14 +3,5 @@ const nextConfig = {}
 
 module.exports = {
     reactStrictMode: true,
-    matcher: [
-      /*
-       * Match all request paths except for the ones starting with:
-       * - api (API routes)
-       * - _next/static (static files)
-       * - _next/image (image optimization files)
-       * - favicon.ico (favicon file)
-       */
-      '/((?!api|_next/static|_next/image|favicon.ico).*)',
-    ],
+
   }
