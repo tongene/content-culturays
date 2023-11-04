718041fbc720545f88d21d3bccd2125c953a7eb2
christina <tochukwu.ngene@sintaxy.com>
2023-11-04 19:49:50 -0100

    content16

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,14 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import getData from '../app/api/hello/route'
-// export default function Home() {
-//   return (
-//     <main className={styles.main}>
-//       <div className={styles.description}>
-//         <p>
-//           Get started by editing&nbsp;
-//           <code className={styles.code}>src/app/page.js</code>
-//         </p>
-      
-//       </div>
-
-       
-//     </main>
-//   )
-// }
 
 
 export default async function Page() {
  const data = await getData(); 
- console.log( data.data.posts.nodes )
+ //console.log( data.data.posts.nodes )
   return (
     <p>
-      
+      hghg
     </p>
   );
 }
