c3c3ada038f9a2db8e505ba290a6220e420e0a32
christina <tochukwu.ngene@sintaxy.com>
2023-11-04 19:18:19 -0100

    content12

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -4,7 +4,8 @@ export default async function getData() {
     const wprest = await fetch('http://content.culturays.com/graphql',{
       method: 'POST',
       headers:{
-      'Content-Type':'application/json'
+      'Content-Type':'application/json',
+      'Content-Length': 1
       },
       body: JSON.stringify({
         query:`
