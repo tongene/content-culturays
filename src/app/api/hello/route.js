f3328fcd814d6e0de0cfc9b9e9dbb4e75074cce4
christina <tochukwu.ngene@sintaxy.com>
2023-11-04 18:39:46 -0100

    content9

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,7 +1,31 @@
  
-
-function route() {
-  return  
-}
-
-export default route
+async function getData() {
+ 
+    const wprest = await fetch('http://content.culturays.com/graphql',{
+      method: 'POST',
+      headers:{
+      'Content-Type':'application/json'
+      },
+      body: JSON.stringify({
+        query:`
+        query WPPOSTS {
+      posts{ 
+        nodes {
+          title
+          slug
+           date
+           content
+           id
+           
+        }
+      }
+       }  
+       ` 
+      
+      })
+      
+      })
+      return wprest.json();
+    }
+     
+  
--- src/app/page.js
+++ src/app/page.js
@@ -1,59 +1,22 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-// export default function Home() {
-//   return (
-//     <main className={styles.main}>
-//       <div className={styles.description}>
-//         <p>
-//           Get started by editing&nbsp;
-//           <code className={styles.code}>src/app/page.js</code>
-//         </p>
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.js</code>
+        </p>
       
-//       </div>
+      </div>
 
       
-//     </main>
-//   )
-// }
+    </main>
+  )
+}
 
  
  
  
-async function getData() {
- 
-const wprest = await fetch('http://content.culturays.com/graphql',{
-  method: 'POST',
-  headers:{
-  'Content-Type':'application/json'
-  },
-  body: JSON.stringify({
-    query:`
-    query WPPOSTS {
-  posts{ 
-    nodes {
-      title
-      slug
-       date
-       content
-       id
-       
-    }
-  }
-   }  
-   ` 
-  
-  })
-  
-  })
-  return wprest.json();
-}
- 
- 
- 
-// This is an async Server Component
-export default async function Page() {
-  const data = await getData();
-console.log(data)
-  return <main>Text content</main>;
-}
