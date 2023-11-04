7d10b232ac2971909c07a007e09f8e341ac44eab
christina <tochukwu.ngene@sintaxy.com>
2023-11-04 19:32:52 -0100

    content14

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,11 +1,11 @@
-
+import axios from "axios";
+import fetch from "node-fetch";
 export default async function getData() {
  
     const wprest = await fetch('http://content.culturays.com/graphql',{
       method: 'POST',
       headers:{
       'Content-Type':'application/json',
-      'Content-Length': 1
       },
       body: JSON.stringify({
         query:`
